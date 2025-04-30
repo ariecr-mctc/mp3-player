1c862e149c6da97e706292c1066957827800ef5b
ariecr-mctc <205052651+ariecr-mctc@users.noreply.github.com>
2025-04-30 07:54:25 -0500

    Split Player class into Player and PlayerGui

--- mp3-player.py
+++ mp3-player.py
@@ -6,22 +6,57 @@ import yt_dlp
 
 
 class Player():
-    def __init__(self, tk_root):
+    def __init__(self):
+        self.instance = vlc.Instance()
+        self.mplayer = self.instance.media_player_new()
+
+    def get_position(self):
+        return self.mplayer.get_position()
+
+    def open(self, uri):
+        if uri.startswith('http'):
+            # Get raw audio URL with yt-dlp. May not work if YouTube breaks something.
+            ydl_opts = {'format': 'bestaudio'}
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                song_info = ydl.extract_info(uri, download=False)
+            if song_info:
+                uri = song_info['url']
+        # Open media with VLC
+        self.mplayer.set_media(self.instance.media_new(uri))
+        self.mplayer.play()
+
+    def pause(self):
+        self.mplayer.pause()
+
+    def play(self):
+        self.mplayer.play()
+
+    def stop(self):
+        self.mplayer.stop()
+
+    def set_position(self, position):
+        self.mplayer.set_position(position)
+
+    def set_volume(self, volume):
+        self.mplayer.audio_set_volume(int(volume))
+
+
+class PlayerGui():
+    def __init__(self, tk_root, player=Player()):
         # Initialize objects
         self.root = tk_root
         self.root.title('Audio Player')
-        self.instance = vlc.Instance()
-        self.player = self.instance.media_player_new()
+        self.player = player
         # Update sliders
         self.job = self.root.after(100, self.update_sliders)
         # Create play/pause button
-        self.play_button = tk.Button(root, text='Play/Pause', command=self.pause)
+        self.play_button = tk.Button(root, text='Play/Pause', command=self.player.pause)
         self.play_button.pack(side=tk.LEFT, padx=5, pady=5)
         # Create stop button
-        self.stop_button = tk.Button(root, text='Stop', command=self.stop)
+        self.stop_button = tk.Button(root, text='Stop', command=self.player.stop)
         self.stop_button.pack(side=tk.LEFT, padx=5, pady=5)
         # Create volume slider
-        self.volume_slider = tk.Scale(root, from_=100, to=0, orient='vertical', command=self.set_volume, label='Volume')
+        self.volume_slider = tk.Scale(root, from_=100, to=0, orient='vertical', command=self.player.set_volume, label='Volume')
         self.volume_slider.set(100)
         self.volume_slider.pack(side=tk.LEFT, padx=5, pady=5)
         # Create position slider
@@ -45,45 +80,22 @@ class Player():
     def open_file(self):
         file_path = tk.filedialog.askopenfilename()
         if file_path:
-            self.player.set_media(self.instance.media_new(file_path))
-            self.player.play()
+            self.player.open(file_path)
 
     def open_url(self):
         url = tk.simpledialog.askstring('Open URL', 'Enter the URL to play:')
-        # Get raw audio URL with yt-dlp. May not work if YouTube breaks something.
-        ydl_opts = {'format': 'bestaudio'}
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            song_info = ydl.extract_info(url, download=False)
-        if song_info:
-            real_url = song_info['url']
-            self.player.set_media(self.instance.media_new(real_url))
-            self.player.play()
-
-    def pause(self):
-        self.player.pause()
-
-    def play(self):
-        self.player.play()
-
-    def stop(self):
-        self.player.stop()
+        self.player.open(url)
 
     def set_position(self, event=None):
         self.player.set_position(float(self.pos_slider.get()) / 100)
         self.job = self.root.after(1000, self.update_sliders)  # Re-enable progress bar updates
-
-    def set_volume(self, volume):
-        self.player.audio_set_volume(int(volume))
 
     def update_sliders(self):
         self.pos_slider.set(self.player.get_position() * 100)
         self.job = self.root.after(1000, self.update_sliders)
 
 
-# player = Player(sys.argv[0])
-# player.vlc.play()
-
 if __name__ == '__main__':
     root = tk.Tk()
-    player = Player(root)
+    playergui = PlayerGui(root)
     root.mainloop()
