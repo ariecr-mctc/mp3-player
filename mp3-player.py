224e493eb5e6ae77ee8052e4fd6fd38371bd017c
ariecr-mctc <205052651+ariecr-mctc@users.noreply.github.com>
2025-05-09 22:55:01 -0500

    Add media display

--- mp3-player.py
+++ mp3-player.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import time
 import tkinter as tk
 from tkinter import filedialog, simpledialog
 import vlc
@@ -9,6 +10,7 @@ class Player():
     def __init__(self):
         self.instance = vlc.Instance()
         self.mplayer = self.instance.media_player_new()
+        self.events = self.mplayer.event_manager()
 
     def get_position(self):
         return self.mplayer.get_position()
@@ -21,9 +23,15 @@ class Player():
                 song_info = ydl.extract_info(uri, download=False)
             if song_info:
                 uri = song_info['url']
-        # Open media with VLC
-        self.mplayer.set_media(self.instance.media_new(uri))
+        # Parse metadata
+        media = self.instance.media_new(uri)
+        vlc.libvlc_media_parse_with_options(media, vlc.MediaParseFlag(network=True), 5000)
+        # Above method is asynchronous, wait until done
+        while not vlc.libvlc_media_get_parsed_status(media):
+            continue
+        self.mplayer.set_media(media)
         self.mplayer.play()
+        return media.get_meta(vlc.Meta.Title)
 
     def pause(self):
         self.mplayer.pause()
@@ -49,27 +57,35 @@ class PlayerGui():
         self.player = player
         # Update sliders
         self.job = self.root.after(100, self.update_sliders)
+        # Create media display
+        self.now_playing_text = tk.StringVar(value='Now Playing: N/A')
+        self.now_playing = tk.Label(self.root, textvariable=self.now_playing_text)
+        self.now_playing.grid(row=0, column=3, sticky='nsew')
         # Create play/pause button
-        self.play_button = tk.Button(root, text='Play/Pause', command=self.player.pause)
-        self.play_button.pack(side=tk.LEFT, padx=5, pady=5)
+        self.play_button = tk.Button(self.root, text='Play/Pause', command=self.player.pause)
+        self.play_button.grid(row=0, column=0)
         # Create stop button
-        self.stop_button = tk.Button(root, text='Stop', command=self.player.stop)
-        self.stop_button.pack(side=tk.LEFT, padx=5, pady=5)
+        self.stop_button = tk.Button(self.root, text='Stop', command=self.stop)
+        self.stop_button.grid(row=0, column=1)
         # Create volume slider
-        self.volume_slider = tk.Scale(root, from_=100, to=0, orient='vertical', command=self.player.set_volume, label='Volume')
+        self.volume_slider = tk.Scale(self.root, from_=100, to=0, orient='vertical', command=self.player.set_volume)
         self.volume_slider.set(100)
-        self.volume_slider.pack(side=tk.LEFT, padx=5, pady=5)
+        self.volume_slider.grid(row=0, column=2, rowspan=2)
+        self.volume_label = tk.Label(self.root, text='Volume')
+        self.volume_label.grid(row=3, column=2, sticky='n')
         # Create position slider
-        self.pos_slider = tk.Scale(root, from_=0, to=100, orient='horizontal')
+        self.pos_slider = tk.Scale(self.root, from_=0, to=100, orient='horizontal', showvalue=False)
         self.pos_slider.bind('<ButtonPress-1>', self.cancel_job)  # Avoid slider position updating while scrubbing
         self.pos_slider.bind('<ButtonRelease-1>', self.set_position)  # Avoid constant audio clipping when scrubbing
-        self.pos_slider.pack(fill=tk.X, padx=10, pady=5)
+        self.pos_slider.grid(row=1, column=3, sticky='nsew')
         # Create open file button
-        self.file_button = tk.Button(root, text='Open File', command=self.open_file)
-        self.file_button.pack(side=tk.LEFT, padx=5, pady=5)
+        self.file_button = tk.Button(self.root, text='Open File', command=self.open_file)
+        self.file_button.grid(row=1, column=0)
         # Create open URL button
-        self.url_button = tk.Button(root, text='Open URL', command=self.open_url)
-        self.url_button.pack(side=tk.LEFT, padx=5, pady=5)
+        self.url_button = tk.Button(self.root, text='Open URL', command=self.open_url)
+        self.url_button.grid(row=1, column=1)
+        self.root.columnconfigure(3, weight=1)
+        self.root.minsize(600, 100)
 
     # Disable slider updates while scrubbing
     def cancel_job(self, event=None):
@@ -79,16 +95,29 @@ class PlayerGui():
 
     def open_file(self):
         file_path = tk.filedialog.askopenfilename()
-        if file_path:
-            self.player.open(file_path)
+        self.open_media(file_path)
+
+    def open_media(self, url):
+        if url:
+            title = self.player.open(url)
+            if title is not None:
+                self.now_playing_text.set('Now Playing: ' + title)
+            else:
+                self.now_playing_text.set('Error: Media info not found.')
 
     def open_url(self):
         url = tk.simpledialog.askstring('Open URL', 'Enter the URL to play:')
-        self.player.open(url)
+        self.now_playing_text.set('YT-DLP is fetching URL, please wait...')
+        self.root.update()
+        self.open_media(url)
 
     def set_position(self, event=None):
         self.player.set_position(float(self.pos_slider.get()) / 100)
         self.job = self.root.after(1000, self.update_sliders)  # Re-enable progress bar updates
+
+    def stop(self):
+        self.player.stop()
+        self.now_playing_text.set('Now Playing: N/A')
 
     def update_sliders(self):
         self.pos_slider.set(self.player.get_position() * 100)
